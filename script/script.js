f23f9c5aea06781fc61484952a766c35b37c59e1
Enes Tasbasi <enestasbasi@outlook.com>
2017-08-02 15:23:41 -0400

    make it responsible (the button doesn't work fully just yet

--- script/script.js
+++ script/script.js
@@ -1,4 +1,9 @@
 
+$("header span").on('click', function() {
+	$( ".nav-bar" ).css('display', 'block');
+	$('body').css('background', 'background: linear-gradient(to right, #d4cccc 175px, transparent 170px)');
+});
+
 $.get("posts/main.html", function(data) {
 	$(".main-visual").html(data);
 });
