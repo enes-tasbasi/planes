bdf01464b904b712bea76f54a65a454166a52668
Enes Tasbasi <enestasbasi@outlook.com>
2017-08-04 12:53:16 -0400

    complete the responsive nav-bar button

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
 
-$("header span").on('click', function() {
-	$( ".nav-bar" ).css('display', 'block');
-	$('body').css('background', 'background: linear-gradient(to right, #d4cccc 175px, transparent 170px)');
-});
+$("header .navButton").on('click', showNavBar);
+
+function showNavBar() {
+	$('section nav').toggleClass('openPanel');
+	$('body').toggleClass('bodyBackground');
+}
 
 $.get("posts/main.html", function(data) {
 	$(".main-visual").html(data);
